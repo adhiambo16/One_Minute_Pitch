0a858484dc3fe84993e98979bc894b949de6c444
Oluoch Lydia <adhiambolydia96@gmail.com>
2018-06-29 17:30:16 +0300

    Added an email column

--- app/models.py
+++ app/models.py
@@ -6,9 +6,10 @@ from flask_login import UserMixin
 class User(db.Model):
     __tablename__ ='users'
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    pass_secure = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
 
     @property
         def password(self):

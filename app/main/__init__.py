ffcf6e6ee799e54a6a1713bdce806f214865d36f
Oluoch Lydia <adhiambolydia96@gmail.com>
2018-06-29 17:02:59 +0300

    created a column in our users table

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,6 +1,6 @@
-from flask import Flask
+# from flask import Flask
 
-def create_app(config_name):
+# def create_app(config_name):
 
-    app = Flask(__name__)
-    return app
+#     app = Flask(__name__)
+#     return app

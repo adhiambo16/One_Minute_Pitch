8dce57f4dc097654fc9578e12843109b5a28d44d
Oluoch Lydia <adhiambolydia96@gmail.com>
2018-06-29 18:12:29 +0300

    created a profile page

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,20 +1,35 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
-from wtforms.validators import Required,Email,EqualTo
+from wtforms.validators import Required,Email,EqualTo,Length
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
 from ..models import User
 
+class RegistrationForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    username = StringField('Enter your username',validators = [Required()])
+    password = PasswordField('Password',validators = [Required(),EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Sign Up')
 
-class PitchForm(FlaskForm):
-    pitch = TextAreaField('Your Pitch', validators=[Required()])
-    # my_category = StringField('Category', validators=[Required()])
-    my_category = SelectField('Category', choices=[('Interview-Pitch','Interview Pitch'),('Product-Pitch','Product Pitch'),('Promotion-Pitch','Promotion Pitch'),('Business','Business'),('Academic','Academic'),('Political','Political'),('Technology','Technology'),('Health','Health')],validators=[Required()])
-    submit = SubmitField('Pitch It!')
+    def validate_email(self,data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError("There is an account with that email")
 
-class CommentForm(FlaskForm):
-    comment = TextAreaField('Comment', validators=[Required()])
-    submit = SubmitField('Post Comment')
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
 
-class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Write something about yourself',validators=[Required()])
-    submit = SubmitField('Submit')
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address', validators=[Required(),Email()])
+    password = PasswordField('Password', validators=[Required()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class ResetPassword(FlaskForm):
+    email = StringField('Email', validators=[Required(), Email()])
+    submit = SubmitField('Reset Password')
+
+class NewPassword(FlaskForm):
+    password = PasswordField('Password',validators=[Required()])
+    password_repeat = PasswordField('Repeat Password', validators=[Required(),EqualTo('password')])
+    submit = SubmitField('Change Password')
--- app/models.py
+++ app/models.py
@@ -7,13 +7,16 @@ from flask_login import UserMixin
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-    
-class User(db.Model):
-    __tablename__ ='users'
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),index = True)
     email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     password_hash = db.Column(db.String(255))
 
     @property

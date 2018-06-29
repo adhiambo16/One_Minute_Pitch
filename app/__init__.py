55623707c410b22515588e9b5dca8301dc4cf075
Oluoch Lydia <adhiambolydia96@gmail.com>
2018-06-29 17:24:05 +0300

    imported login manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 db=SQLAlchemy()
 def create_app(config_name):
     app = Flask(__name__)
 
     db.init_app(app)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
     return app

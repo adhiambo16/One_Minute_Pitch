b097c8242ebce29e8f97f65adebcd29a0b0933ce
Oluoch Lydia <adhiambolydia96@gmail.com>
2018-07-02 15:15:27 +0300

    Deploying to heroku

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 # export MAIL_USERNAME=<Your Email Address>
 # export MAIL_PASSWORD=<Your Email Password>
+export
+DATABASE_URL = 'postgres://medhhizyjwvfbn:64cf1a2ed0c38960d99e89b5853261f425c9e3d39ca8fda8961f5dbabd9723f6@ec2-54-243-253-24.compute-1.amazonaws.com:5432/d3jk9q8o8b9fjc'
+
 export SECRET_KEY='Lydia'
 python3.6 manage.py server
